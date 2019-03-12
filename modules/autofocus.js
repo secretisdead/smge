6127c9046d9800eba750fb3734d0945e13186527
secret <secretisdead@gmail.com>
2019-03-12 00:52:49 -0700

    run focus in all steps

--- modules/autofocus.js
+++ modules/autofocus.js
@@ -3,7 +3,22 @@
 import { Focus } from './focus.js';
 
 export class Autofocus extends Focus {
+	early_update() {
+		this.focus();
+	}
 	update() {
+		this.focus();
+	}
+	late_update() {
+		this.focus();
+	}
+	early_draw() {
+		this.focus();
+	}
+	draw() {
+		this.focus();
+	}
+	late_draw() {
 		this.focus();
 	}
 }
