d81e4b58a74ba5d79db232335515517f4ab356f4
secret <secretisdead@gmail.com>
2019-05-03 02:37:15 -0700

    add optional callbacks to scene transitions

--- game_objects/scene.js
+++ game_objects/scene.js
@@ -12,10 +12,7 @@ export class Scene extends GameObject {
 		this.options = options || {};
 		this.cover = null;
 	}
-	/** /
-	transition(source_object, remove_source_object, cb) {
-	/**/
-	transition(source_object, remove_source_object) {
+	transition(source_object, remove_source_object, callbacks) {
 		this.load();
 
 		source_object = source_object || null;
@@ -27,6 +24,22 @@ export class Scene extends GameObject {
 		this.cover_type_out = this.options.cover_type_out || 'cut';
 		this.cover_duration_out = this.options.cover_duration_out || 1;
 		this.min_cover_duration = this.options.min_cover_duration || 1;
+		this.callbacks = {
+			cover_in: null,
+			composed: null,
+			cover_out: null,
+		};
+		if (callbacks) {
+			if (callbacks.cover_in) {
+				this.callbacks.cover_in = callbacks.cover_in;
+			}
+			if (callbacks.composed) {
+				this.callbacks.composed = callbacks.composed;
+			}
+			if (callbacks.cover_out) {
+				this.callbacks.composed = callbacks.cover_out;
+			}
+		}
 
 		if (this.cover) {
 			this.remove_module(this.cover);
@@ -41,6 +54,10 @@ export class Scene extends GameObject {
 			// after cover is in set covered
 			() => {
 				console.log('scene finish cover in');
+				if (this.callbacks.cover_in) {
+					console.log('cover in callback provided, running now');
+					this.callbacks.cover_in(this);
+				}
 				/** /
 				// run auxillary callback if specified
 				if (cb && 'function' == typeof cb) {
@@ -79,6 +96,10 @@ export class Scene extends GameObject {
 	}
 	compose() {
 		this.composed = true;
+		if (this.callbacks.composed) {
+			console.log('composed callback provided, running now');
+			this.callbacks.composed(this);
+		}
 		console.log('scene waiting min duration: ' + this.min_cover_duration + ' before starting cover out');
 		this.smge.add_waiting_action(() => {
 			console.log('scene start cover out');
@@ -88,6 +109,10 @@ export class Scene extends GameObject {
 				() => {
 					console.log('scene finish cover out');
 					this.remove_module(this.cover);
+					if (this.callbacks.cover_out) {
+						console.log('cover out callback provided, running now');
+						this.callbacks.cover_out(this);
+					}
 				}
 			);
 		}, this.min_cover_duration);
